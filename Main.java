c26314dd71b7ddcdf7cccf1f8d7003c6a0b2734a
yasir <yasir.arafat3@gmail.com>
2024-04-29 16:39:54 +0530

    comitting java file

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Java program for jenkins demo");
+        System.out.println("Todays Date is : " + new Date());
+    }
+}
